e68f4b1f047a907d2d5101855beecb1a967300d7
Carlos Soria <soriac367@gmail.com>
2024-04-13 22:26:52 -0500

    Concatenation

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a function named 'cat_arrays'"""
+
+
+def cat_arrays(arr1, arr2):
+    """Functions to concatenate two arrays"""
+    return arr1 + arr2
