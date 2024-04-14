7df036d676c94739c8d935c11803e7cf6b18174f
Carlos Soria <soriac367@gmail.com>
2024-04-14 00:53:07 -0500

    Matrix Operations NUmpy

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a function named 'np_elementwise'"""
+
+
+def np_elementwise(mat1, mat2):
+    """Function to compute operations in a matrix"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
