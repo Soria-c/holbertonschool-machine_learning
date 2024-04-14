efeb7f21a790b2ccd3fb2bc4bbaccc28587b8bb3
Carlos Soria <soriac367@gmail.com>
2024-04-13 20:44:47 -0500

    Dimensions

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,8 @@
+"""This module defines a function named 'matrix_shape'"""
+
+
+def matrix_shape(matrix):
+    """Functions to calculate de dimensions of a matrix"""
+    if (isinstance(matrix, list)):
+        return [len(matrix), *matrix_shape(matrix[0])]
+    return []
