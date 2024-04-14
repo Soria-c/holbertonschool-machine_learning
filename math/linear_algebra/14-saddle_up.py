d282ebe1af75b17135d6ed8242184c369896d30a
Carlos Soria <soriac367@gmail.com>
2024-04-14 01:00:43 -0500

    Matrix MUltiplication numpy

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a function named 'np_matmul'"""
+
+
+def np_matmul(mat1, mat2):
+    """Function to compute matrix multiplication"""
+    return mat1 @ mat2
