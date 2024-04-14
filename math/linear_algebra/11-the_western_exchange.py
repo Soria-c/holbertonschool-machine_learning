6b15e8c7430328fe843fa26d13edceb168d25e3f
Carlos Soria <soriac367@gmail.com>
2024-04-14 00:47:54 -0500

    Matrix Transpose Numpy

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """This module defines a function named 'np_transpose'"""
-import numpy as np
 
 
 def np_transpose(matrix):
     """Function to transpose a matrix"""
-    return np.transpose(matrix)
+    return matrix.transpose()
