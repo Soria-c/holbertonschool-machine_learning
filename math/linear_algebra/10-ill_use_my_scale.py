7cc647cb88e9a61537dc1dbe9ebeccba247743f1
Carlos Soria <soriac367@gmail.com>
2024-04-14 00:37:52 -0500

    Matrix Shape numpy

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module defines a function named 'np_shape'"""
+
+
+def np_shape(matrix):
+    """Function to compute the shape of a matrix"""
+    return matrix.shape
