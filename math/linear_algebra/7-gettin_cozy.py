4c77b6fc811b9ea16fe17ee4d3055f593221be02
Carlos Soria <soriac367@gmail.com>
2024-04-13 23:21:37 -0500

    Matrix Concatenation

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,7 +4,11 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Functions to concatenate two matrices in a given axis"""
-    if (axis == 0):
+    l1 = len(mat1)
+    l2 = len(mat2)
+    if (l2 == 0 or l1 == 0):
+        return
+    if (axis == 0) and (len(mat1[0]) == len(mat2[0])):
         return [*[list(i) for i in mat1], *[list(i) for i in mat2]]
-    elif (axis == 1):
-        return [[*mat1[i], *mat2[i]] for i in range(len(mat1))]
+    if (axis == 1) and (l1 == l2):
+        return [[*mat1[i], *mat2[i]] for i in range(l1)]
