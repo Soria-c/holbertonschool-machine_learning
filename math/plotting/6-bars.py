bd7b8dc2af8e0f3fa5b78c342d74649154b19a14
Carlos Soria <soriac367@gmail.com>
2024-04-21 10:54:17 -0500

    Stacked bar chart

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -9,7 +9,7 @@ def bars():
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
-    persons = ["Frrah", "Fred", "Felicia"]
+    persons = ["Farrah", "Fred", "Felicia"]
     colors = ("r", "yellow", "#ff8000", "#ffe5b4")
     plt.title("Number of Fruit per Person")
     plt.ylabel("Quantity of Fruit")
