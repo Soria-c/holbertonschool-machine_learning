c75b3df188b32e7becbfa308cafe14e4b154a1d3
Carlos Soria <soriac367@gmail.com>
2024-04-21 14:19:17 -0500

    Calculus

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -7,4 +7,4 @@ def summation_i_squared(n):
     """Function to calculate a sum"""
     if (isinstance(n, int)):
         arr = np.arange(1, n+1)
-        return np.power(arr, 2).sum()
+        return int(np.power(arr, 2).sum())
