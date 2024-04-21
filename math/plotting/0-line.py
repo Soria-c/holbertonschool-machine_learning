a8184cd8e28521aeb3eec466e4bdbbb30d79f2c9
Carlos Soria <soriac367@gmail.com>
2024-04-21 00:12:22 -0500

    Line graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
+"""Matplotlib tasks"""
 import numpy as np
 import matplotlib.pyplot as plt
-"""Matplotlib tasks"""
+
 
 
 def line():
     """Fuction to plot a curve"""
+
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.gca().set_xlim([0, 10])
