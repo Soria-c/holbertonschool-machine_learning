4286966ed31fab8843c181f576c051b20c58a7ab
Carlos Soria <soriac367@gmail.com>
2024-04-21 15:30:25 -0500

    Calculus

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -6,7 +6,7 @@ def poly_integral(poly, C=0):
     """Function that calculates the integral of a polynomial"""
     if not (isinstance(poly, list)) or (len(poly) == 0):
         return None
-    if not (isinstance(C, [int, float])):
+    if not (isinstance(C, (int, float))):
         return None
     if (len(poly) == 1) and (poly[0] == 0):
         return [C]
