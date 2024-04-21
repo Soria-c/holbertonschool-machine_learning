eec8fb94a8aad91a2685dca5a69e30a114a28092
Carlos Soria <soriac367@gmail.com>
2024-04-21 14:59:46 -0500

    Calculus

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -7,13 +7,8 @@ def poly_derivative(poly):
     if not(isinstance(poly, list) or len(poly) == 0):
         return None
     result = []
-    degree = False
     for i, x in enumerate(poly):
-        value = i * x
-        if (value != 0):
-            degree = True
-            result.append(value)
-        elif (degree):
-            result.append(value)
+        if (i >= 1):
+            result.append(i * x)
     result = [0] if len(result) == 0 else result
     return result
