3419a6a3f2707305673b9905d0259359f60ac78e
Carlos Soria <soriac367@gmail.com>
2024-04-21 00:05:43 -0500

    Line graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -8,7 +8,7 @@ def line():
     """Fuction to plot a curve"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
+    plt.axis((0,10,0,y[-1]))
     x = np.arange(11)
     plt.plot(x, y, "r-")
     plt.show()
