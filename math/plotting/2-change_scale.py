ce56f7ec7d074755ecb18120995e5ef5431d6209
Carlos Soria <soriac367@gmail.com>
2024-04-21 08:45:56 -0500

    Line graph with log scaling

---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Matplotlib tasks"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def change_scale():
+    """Function to plot a line graph"""
+    x = np.arange(0, 28651, 5730)
+    r = np.log(0.5)
+    t = 5730
+    y = np.exp((r / t) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.title("Exponential Decay of C-14")
+    plt.ylabel("Fraction Remaining")
+    plt.xlabel("Time (years)")
+    plt.yscale("log")
+    plt.gca().set_xlim([0, 28650])
+    plt.plot(x, y)
+    plt.show()
