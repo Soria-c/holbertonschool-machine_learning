ba455f4274db6c52dbe055bde3f59f55c54cec35
Carlos Soria <soriac367@gmail.com>
2024-05-07 16:17:03 -0500

    One-Hot Decode

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""This module defines a function"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+    Converts a one-hot matrix into a vector of labels.
+    ```
+    Parameters:
+    -----------
+    one_hot: numpy.ndarray
+         is a one-hot encoded numpy.ndarray with shape (classes, m)
+    Returns:
+    --------
+        a numpy.ndarray with shape (m, ) containing
+        the numeric labels for each example, or None on failure
+    """
+    if type(one_hot) is not np.ndarray:
+        return
+    try:
+        return np.where(one_hot.transpose() > 0)[1]
+    except Exception:
+        return
