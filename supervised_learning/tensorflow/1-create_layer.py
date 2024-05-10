93b501eff8560685014d3b911e9743f4f7aeba0b
Carlos Soria <soriac367@gmail.com>
2024-05-10 10:44:16 -0500

    Layer

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,6 +5,8 @@ import tensorflow.compat.v1 as tf
 
 def create_layer(prev, n, activation):
     """Function to create a tensorflow layer"""
-    y = tf.keras.layers.Dense(n, activation=activation, name="layer")
-    tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    init = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    y = tf.keras.layers.Dense(n,
+                              activation=activation, name="layer",
+                              kernel_initializer=init)
     return y(prev)
