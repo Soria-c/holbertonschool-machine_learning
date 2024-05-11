a5a67b8d4464db41a8bf1392e05b40de40ffdba1
Carlos Soria <soriac367@gmail.com>
2024-05-11 09:54:13 -0500

    Accuracy

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -4,7 +4,11 @@ import tensorflow.compat.v1 as tf
 
 
 def create_placeholders(nx, classes):
-    """Function that returns two placeholders, x and y,
-    for the neural network"""
+    """
+    Function that returns two placeholders, x and y,
+    for the neural network
+    x is the placeholder for the input data to the neural network
+    y is the placeholder for the one-hot labels for the input data
+    """
     return tf.placeholder(dtype=tf.float32, shape=[None, nx], name="x"), \
         tf.placeholder(dtype=tf.float32, shape=(None, classes), name="y")
---
+++ supervised_learning/tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Calculate Accuracy"""
+# import tensorflow.compat.v1 as tf
+import tensorflow
+tf = tensorflow.compat.v1
+
+
+create_layer = __import__('1-create_layer').create_layer
+
+
+def calculate_accuracy(y, y_pred):
+    """
+    Function that creates the forward propagation graph for the neural network
+    ```
+    Parameters:
+    -----------
+        y is a placeholder for the labels of the input data
+        y_pred is a tensor containing the network’s predictions
+    """
+    labels = tf.math.argmax(y, axis=1)
+    prediction = tf.math.argmax(y_pred, axis=1)
+    correct_predictions_encoded = tf.cast(tf.math.equal(
+        labels, prediction), tf.float32)
+    return tf.math.reduce_mean(correct_predictions_encoded)
