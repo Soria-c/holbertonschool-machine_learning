5937b78fbe994bc8cea2a101492d128061bfdf3c
Carlos Soria <soriac367@gmail.com>
2024-05-19 08:02:41 -0500

    RMSprop upgraded

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""RMSProp Upgraded"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    Sets up the RMSProp optimization algorithm in TensorFlow
+    ```
+    Parameters:
+    -----------
+    alpha: float
+        the learning rate
+    beta2: float
+        the momentum weight
+    epsilon: float
+        is a small number to avoid division by zero
+    Return:
+    -------
+    optimizer
+    """
+    return tf.keras.optimizers.RMSprop(
+        learning_rate=alpha, momentum=beta2, epsilon=epsilon)
