2b7db6088c86c2ff8f644559eef9ef94ef95464d
Carlos Soria <soriac367@gmail.com>
2024-05-18 21:24:57 -0500

    Momentum Op

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Create Momemtum Op"""
+import tensorflow.compat.v1 as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    Updates a variable using the gradient descent
+    with momentum optimization algorithm
+    ```
+    Parameters:
+    -----------
+    alpha: float
+        the learning rate
+    beta1: float
+        the momentum weight
+    Return:
+    -------
+    optimizer
+    """
+    return tf.train.MomentumOptimizer(alpha, beta1)
