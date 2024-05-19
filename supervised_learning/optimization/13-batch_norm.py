7b12e3da2436afe045a5a574cc5f24cce80330f2
Carlos Soria <soriac367@gmail.com>
2024-05-19 11:03:12 -0500

    Learning rate decay

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Learning Rate Decay"""
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    Updates the learning rate using inverse time decay
+    ```
+    Parameters:
+    -----------
+    Z: numpy.ndarray(m, n)
+        m is the number of data points
+        n is the number of features in Z
+    gamma: numpy.ndarray(1, n)
+        containsthe scales used for batch normalization
+    beta: numpy.ndarray(1, n)
+        contains the offsets used for batch normalization
+    epsilon: float
+        small number used to avoid division by zero
+    Return:
+    -------
+    normalized Z matrix
+    """
+    mean = np.mean(Z, axis=0)
+    variance = np.std(Z, axis=0)**2
+    Z_norm = (Z - mean) / ((variance + epsilon) ** (1 / 2.0))
+    return gamma * Z_norm + beta
