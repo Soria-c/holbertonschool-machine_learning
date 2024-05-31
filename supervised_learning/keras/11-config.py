197e905fb835d425f644fbc9fb6ab9e8efa46ddc
Carlos Soria <soriac367@gmail.com>
2024-05-31 16:30:24 -0500

    Save and Load Configuration

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Save and Load Configuration"""
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """
+    Saves a model’s configuration in JSON format
+    """
+    with open(filename, 'w') as f:
+        f.write(network.to_json())
+
+
+def load_config(filename):
+    """
+    Loads a model with a specific configuration
+    """
+    with open(filename, 'r') as f:
+        return f.read()
