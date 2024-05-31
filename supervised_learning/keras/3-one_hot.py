b49413ff61ecf2a016b7fd3e7f8bf02143b56ab9
Carlos Soria <soriac367@gmail.com>
2024-05-31 14:21:28 -0500

    One Hot

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""One Hot"""
+# import tensorflow.keras as K
+import keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Converts a label vector into a one-hot matrix
+    ```
+    Parameters:
+    -----------
+    labels:
+    classes:
+    """
+    return K.utils.to_categorical(labels, classes)
