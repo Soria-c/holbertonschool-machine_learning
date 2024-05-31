1fb2fb5b2d9e227cf10538056a5eed0285d44595
Carlos Soria <soriac367@gmail.com>
2024-05-31 16:37:24 -0500

    TEst

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Test"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    Tests a neural network
+    """
+    return network.evaluate(
+        x=data,
+        y=labels,
+        verbose=verbose
+    )
