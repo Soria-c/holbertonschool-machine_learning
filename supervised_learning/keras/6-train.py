72a25e3b30c059d894d36c44527d629ea2a58341
Carlos Soria <soriac367@gmail.com>
2024-05-31 15:02:21 -0500

    Early Stopping

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Early Stopping"""
+# import tensorflow.keras as K
+import keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None,
+                early_stopping=False, patience=0, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent
+    """
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=[
+            K.callbacks.EarlyStopping(
+                patience=patience
+            )
+        ] if validation_data else None
+    )
