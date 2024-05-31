6def2379d86e77d134e9aff7e27dfbea533bb37c
Carlos Soria <soriac367@gmail.com>
2024-05-31 14:53:02 -0500

    Validate

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Validate"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent
+    """
+    return network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data
+    )
