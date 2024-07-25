54c33de2e5d665627fbab0fca7c0a8285aa8faa4
Carlos Soria <soriac367@gmail.com>
2024-07-25 12:05:10 -0500

    Normal

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -20,3 +20,15 @@ class Normal:
             self.mean = float(sum(data)/len(data))
             self.variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(self.variance ** 0.5)
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value.
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score.
+        """
+        return (z * self.stddev) + self.mean
