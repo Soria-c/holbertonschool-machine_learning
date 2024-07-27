3362bbf06b0120dd161735cb7d367ff398d78c51
Carlos Soria <soriac367@gmail.com>
2024-07-27 10:22:29 -0500

    Mean and Covariance

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Mean and Covariance"""
+
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    Calculates the mean and covariance of a data set
+    """
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0, keepdims=True)
+
+    X_C = X - mean
+    cov = np.dot(X_C.T, X_C) / (X.shape[0] - 1)
+    return mean, cov
