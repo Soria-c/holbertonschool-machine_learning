0fc92b07d8a2c9610c89fdd3e38976c39e3831bb
Carlos Soria <soriac367@gmail.com>
2024-07-27 10:36:09 -0500

    Correlation

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Correlation"""
+
+import numpy as np
+
+
+def correlation(C):
+    """
+    Calculates a correlation matrix
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+    if C.ndim != 2 or C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    stds = np.sqrt(np.diag(C))
+    return C / np.outer(stds, stds)
