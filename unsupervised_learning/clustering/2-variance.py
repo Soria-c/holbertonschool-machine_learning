cb50b5c27f1a71b55baceda0d831c38b6d3280d7
Carlos Soria <soriac367@gmail.com>
2024-08-01 08:48:00 -0500

    Variance

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -18,4 +18,10 @@ def variance(X, C):
     """
     Calculates the total intra-cluster variance for a data set
     """
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+    if type(C) is not np.ndarray or C.ndim != 2:
+        return None
+    if C.shape[1] != X.shape[1]:
+        return None
     return ((X-C[get_min_index(X, C)])**2).sum()
