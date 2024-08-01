b90960f7209b8e847bf81f071885426e791b7125
Carlos Soria <soriac367@gmail.com>
2024-08-01 08:35:24 -0500

    K-means

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -16,6 +16,17 @@ def initialize(X, k):
         return
 
 
+def get_min_index(X, C):
+    """
+    Get indices of closests centroids
+    """
+    points, dims = X.shape
+    x = X.reshape(points, 1, dims)
+    r = np.repeat(C[np.newaxis, ...], points, axis=0)
+    dist = np.linalg.norm(x - r, axis=2)
+    return np.argmin(dist, axis=1)
+
+
 def kmeans(X, k, iterations=1000):
     """
     Performs K-means on a dataset
@@ -26,27 +37,18 @@ def kmeans(X, k, iterations=1000):
     if ic_centroids is None:
         return None, None
     for _ in range(iterations):
-        # ric_c = np.repeat(ic_centroids, X.shape[0], axis=0)
-        # b = np.vsplit(ric_c, k)
-        # d = []
-        # for i in range(k):
-        #     dis = np.linalg.norm(X-b[i], axis=1)
-        #     d.append(dis)
-        # index = np.array(d).argmin(axis=0)
-        points, dims = X.shape
-        x = X.reshape(points, 1, dims)
-        r = np.repeat(ic_centroids[np.newaxis, ...], points, axis=0)
-        dist = np.linalg.norm(x - r, axis=2)
-        index = np.argmin(dist, axis=1)
+        index = get_min_index(X, ic_centroids)
+        means = np.zeros(ic_centroids.shape)
 
         sort_index_i = np.unique(np.sort(index), return_index=True)
         s = np.split(X[np.argsort(index)], sort_index_i[1])
-        miss = np.setdiff1d(np.arange(k), sort_index_i[0])
-        means = np.zeros(ic_centroids.shape)
+
         means[sort_index_i[0]] = np.array(list(map(
             lambda x: np.mean(x, axis=0), s[1:])))
-        re_init = initialize(X, 1)
+
+        miss = np.setdiff1d(np.arange(k), sort_index_i[0])
         if (len(miss) > 0):
+            re_init = initialize(X, 1)
             means[miss] = re_init[0]
         if (np.array_equal(ic_centroids, means)):
             return ic_centroids, index
--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -3,30 +3,19 @@
 import numpy as np
 
 
+def get_min_index(X, C):
+    """
+    Get indices of closests centroids
+    """
+    points, dims = X.shape
+    x = X.reshape(points, 1, dims)
+    r = np.repeat(C[np.newaxis, ...], points, axis=0)
+    dist = np.linalg.norm(x - r, axis=2)
+    return np.argmin(dist, axis=1)
+
+
 def variance(X, C):
     """
     Calculates the total intra-cluster variance for a data set
     """
-    # ric_c = np.repeat(C, X.shape[0], axis=0)
-    points, dims = X.shape
-    x = X.reshape(points, 1, dims)
-    
-    print(X.shape)
-    # print(x[:5])
-    # print(X[:5])
-    r = np.repeat(C[np.newaxis, ...], points, axis=0)
-    print(r[:1])
-    print(C)
-    print(r.shape)
-    print(x.shape)
-    print(C.shape)
-    print(x[:1])
-    x = x - r
-    # print(x.shape)
-    # print(x[:1])
-    dist = np.linalg.norm(x, axis=2)
-    print(dist.shape)
-    print(dist[:1])
-
-    mins = np.argmin(dist, axis=1)
-    print(mins)
+    return ((X-C[get_min_index(X, C)])**2).sum()
