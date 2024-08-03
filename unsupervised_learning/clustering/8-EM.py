df51a03e03b6a5cd8f2b2dc63f512b850771dc68
Carlos Soria <soriac367@gmail.com>
2024-08-03 17:59:28 -0500

    EM

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -16,6 +16,8 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         return None, None, None, None, None
     if type(tol) is not float or tol < 0:
         return None, None, None, None, None
+    if type(verbose) is not bool:
+        return None, None, None, None, None
     pi, m, S = initialize(X, k)
     if pi is None:
         return None, None, None, None, None
