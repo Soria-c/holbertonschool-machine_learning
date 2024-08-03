46b15fa7095b588a66d4656314038f8e15aa36c7
Carlos Soria <soriac367@gmail.com>
2024-08-03 17:21:12 -0500

    EM

---
+++ unsupervised_learning/clustering/8-EM.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Maximization"""
+import numpy as np
+
+
+initialize = __import__('4-initialize').initialize
+expectation = __import__('6-expectation').expectation
+maximization = __import__('7-maximization').maximization
+
+
+def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
+    """
+    Performs the expectation maximization for a GMM
+    """
+    pi, m, S = initialize(X, k)
+    if pi is None:
+        return None, None, None, None, None
+    for i in range(iterations):
+        if ((E := expectation(X, pi, m, S))[0] is not None) and\
+           ((M := maximization(X, E[0]))[0] is not None):
+            g, log_l = E
+            pi, m, S = M
+            if verbose and not (i % 10):
+                print(f"Log Likelihood after {i} iterations: {log_l}")
+        else:
+            return None, None, None, None, None
+        if (i) and (log_l - likelihood <= tol):
+            if verbose and (i % 10):
+                print(f"Log Likelihood after {i} iterations: {log_l}")
+            return pi, m, S, g, log_l
+        likelihood = log_l
+    return pi, m, S, g, log_l
