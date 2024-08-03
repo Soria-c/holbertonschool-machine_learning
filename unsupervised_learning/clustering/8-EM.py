b165e2b7d11fa100beb72942aad19be9ee93a0b9
Carlos Soria <soriac367@gmail.com>
2024-08-03 18:57:54 -0500

    EM

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -35,7 +35,7 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
         else:
             return None, None, None, None, None
         likelihood = log_l
-        g, log_l = expectation(X, pi, m, S)
+    g, log_l = expectation(X, pi, m, S)
     if verbose:
         print(f"Log Likelihood after {iterations} iterations: {log_l:.5f}")
     return pi, m, S, g, log_l
