2b36b5c9a44130d3f2ae418e6c16a344e5799868
Carlos Soria <soriac367@gmail.com>
2024-08-03 16:02:34 -0500

    M step

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Maximization"""
+import numpy as np
+
+
+def maximization(X, g):
+    """
+    Calculates the maximization step in the EM algorithm for a GMM
+    """
+    if (not (isinstance(X, np.ndarray)) or X.ndim != 2) or\
+       (not (isinstance(g, np.ndarray)) or X.ndim != 2):
+        return None, None, None
+    pi = []
+    m = []
+    S = []
+    for k in range(len(g)):
+        pi.append(np.sum(g[k] / len(X)))
+        denm = np.sum(g[k])
+        uk = np.sum(g[k][..., np.newaxis] * X, axis=0) / denm
+        m.append(uk)
+        diff = X-uk
+        S.append(diff.T @ (g[k][..., np.newaxis] * diff) / denm)
+    return np.array(pi), np.array(m), np.array(S)
