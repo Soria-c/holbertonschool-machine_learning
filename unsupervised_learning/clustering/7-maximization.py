378b3e96e9adea2fb75ba15c5872a13acfdc5711
Carlos Soria <soriac367@gmail.com>
2024-08-03 16:26:34 -0500

    M step

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -10,7 +10,9 @@ def maximization(X, g):
     if (not (isinstance(X, np.ndarray)) or X.ndim != 2) or\
        (not (isinstance(g, np.ndarray)) or g.ndim != 2):
         return None, None, None
-    if  g.shape[1] != X.shape[0]:
+    if g.shape[1] != X.shape[0]:
+        return None, None, None
+    if not np.isclose(np.sum(g, axis=0), [1])[0]:
         return None, None, None
     pi = []
     m = []
