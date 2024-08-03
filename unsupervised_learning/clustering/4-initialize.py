b5dc489ccf97992f0bb6c3282fd84ac7ddc17b99
Carlos Soria <soriac367@gmail.com>
2024-08-03 09:35:37 -0500

    Initialize GMM

---
+++ unsupervised_learning/clustering/4-initialize.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Initialize GMM"""
+import numpy as np
+
+
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """
+    Initializes variables for a Gaussian Mixture Model
+    """
+    n, d = X.shape
+    pi = np.array([1 / k]).repeat(k)
+    if ((k_m := kmeans(X, k))[0] is not None):
+        m = k_m[0]
+    else:
+        return None, None, None
+    S = np.identity(d)[np.newaxis, ...].repeat(k, axis=0)
+    return pi, m, S
