14ee0e6f119adbe9c32495351ea362af3ce0ab76
Carlos Soria <soriac367@gmail.com>
2024-08-03 14:33:22 -0500

    E step

--- unsupervised_learning/clustering/6-expectation.py
+++ unsupervised_learning/clustering/6-expectation.py
@@ -15,6 +15,8 @@ def expectation(X, pi, m, S):
         return None, None
     if not (isinstance(S, np.ndarray)) or S.ndim != 3:
         return None
+    if (pi.shape[0] != S.shape[0]):
+        return None, None
     if not np.isclose([np.sum(pi)], [1])[0]:
         return None, None
 
