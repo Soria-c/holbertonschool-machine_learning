dc076ff26f0686b671b42328fdc5e924f906550a
Carlos Soria <soriac367@gmail.com>
2024-08-04 15:54:04 -0500

    Linear Algebra

---
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Slice Like A Ninja"""
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """
+    Slices a matrix along specific axes
+    """
+    ndim = matrix.ndim
+    indexing = tuple([slice(*axes.get(i, (None, None))) for i in range(ndim)])
+    return matrix[indexing]
