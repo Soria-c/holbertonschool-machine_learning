b5fd8841b788df6b181a7ef097e38b9ed4c9d39e
Carlos Soria <soriac367@gmail.com>
2024-11-27 19:31:16 -0500

    Array

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""To numpy array"""
+import pandas as pd
+
+
+def array(df: pd.DataFrame):
+    """
+    Selects the last 10 rows of the 'High' and 'Close' columns
+    from the input DataFrame,
+    and converts these values into a numpy.ndarray.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame containing 'High'
+    and 'Close' columns.
+
+    Returns:
+    np.ndarray: A numpy array containing the last 10 rows
+    of the 'High' and 'Close' columns.
+    """
+    # Select the last 10 rows of the 'High' and 'Close' columns
+    selected_data = df[['High', 'Close']].tail(10)
+
+    # Convert the selected data into a numpy ndarray
+    result_array = selected_data.to_numpy()
+
+    return result_array
