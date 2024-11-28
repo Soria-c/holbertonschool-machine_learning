8402e47c145a22b28628a3e64c0c31a00f035bec
Carlos Soria <soriac367@gmail.com>
2024-11-27 19:42:36 -0500

    Array

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Slicing"""
+
+
+def slice(df):
+    """
+    Extracts the 'High', 'Low', 'Close', and 'Volume_BTC'
+    columns from the input DataFrame,
+    selects every 60th row, and returns the resulting sliced DataFrame.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame containing
+            'High', 'Low', 'Close', and 'Volume_BTC' columns.
+    Returns:
+    pd.DataFrame: A DataFrame containing every 60th row
+        from the selected columns.
+    """
+    # Extract the specified columns
+    selected_columns = df[['High', 'Low', 'Close', 'Volume_BTC']]
+
+    # Select every 60th row using slicing
+    sliced_df = selected_columns.iloc[::60]
+
+    return sliced_df
